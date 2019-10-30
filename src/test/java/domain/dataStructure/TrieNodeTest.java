ecd12498d221f9c5236e6eb448879053b6e996ad
Luis Oriol Soler Cruz <luisoriol.solercruz@gmail.com>
2019-10-30 17:30:43 +0100

    Add unit test for HashTrie dictionary

--- src/test/java/domain/dataStructure/TrieNodeTest.java
+++ src/test/java/domain/dataStructure/TrieNodeTest.java
@@ -2,12 +2,12 @@ package domain.dataStructure;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
-@Ignore
 public class TrieNodeTest {
-    private static final String word = "example";
+    private static final String WORD_VALID = "example";
+    private static final int INDEX_INVALID = -1;
+
 
     private TrieNode node;
 
@@ -16,58 +16,60 @@ public class TrieNodeTest {
         node = new TrieNode((char) 0);
     }
 
-    /*// ************************************ INSERT ************************************
+    // ************************************ INSERT ************************************
     @Test
-    public void verify_insert_returnsFalse_whenWordIsNull() {
+    public void verify_insert_returnsInvalidIndex_whenWordIsNull() {
         int response = this.node.insert(null, 0, 1);
 
-        Assert.assertTrue(response == -1);
+        Assert.assertEquals(INDEX_INVALID, response);
     }
 
     @Test
-    public void verify_insert_returnsFalse_whenWordIsEmpty() {
+    public void verify_insert_returnsInvalidIndex_whenWordIsEmpty() {
         int response = this.node.insert("", 0, 1);
 
-        Assert.assertTrue(response == -1);
+        Assert.assertEquals(INDEX_INVALID, response);
     }
 
     @Test
-    public void verify_insert_returnsFalse_whenWordIsBlank() {
+    public void verify_insert_returnsInvalidIndex_whenWordIsBlank() {
         int response = this.node.insert("   ", 0, 1);
 
-        Assert.assertTrue(response == -1);
+        Assert.assertEquals(INDEX_INVALID, response);
     }
 
     @Test
-    public void verify_insert_returnsFalse_whenPositionIsGreaterOrEqual_thanWordLength() {
-        boolean response = this.node.insert(this.word, this.word.length(), 1;
+    public void verify_insert_returnsInvalidIndex_whenPositionIsGreaterOrEqual_thanWordLength() {
+        int response = this.node.insert(WORD_VALID, WORD_VALID.length(), 1);
 
-        Assert.assertFalse(response);
+        Assert.assertEquals(INDEX_INVALID, response);
     }
 
     @Test
-    public void verify_insert_returnsFalse_whenPositionIsNegative() {
-        boolean response = this.node.insert(this.word, -1);
+    public void verify_insert_returnsInvalidIndex_whenPositionIsNegative() {
+        int response = this.node.insert(WORD_VALID, -1, 1);
 
-        Assert.assertFalse(response);
+        Assert.assertEquals(INDEX_INVALID, response);
     }
 
     @Test
-    public void verify_insert_returnsTrue_whenPositionIsLastChar() {
-        boolean response = this.node.insert(this.word, this.word.length() - 1);
+    public void verify_insert_returnsValidIndex_whenPositionIsLastChar() {
+        int validIndex = 1;
+        int response = this.node.insert(WORD_VALID, WORD_VALID.length() - 1, validIndex);
 
-        String lastChar = String.valueOf(this.word.charAt(this.word.length() - 1));
+        String lastChar = String.valueOf(WORD_VALID.charAt(WORD_VALID.length() - 1));
 
-        Assert.assertTrue(response);
+        Assert.assertEquals(validIndex, response);
         Assert.assertNotNull(this.node.lookup(lastChar, 0));
     }
 
     @Test
-    public void verify_insert_returnsTrue_whenInsertsAValidAndFullWord() {
-        boolean response = this.node.insert(this.word, 0);
+    public void verify_insert_returnsValidIndex_whenInsertsAValidAndFullWord() {
+        int validIndex = 1;
+        int response = this.node.insert(WORD_VALID, 0, validIndex);
 
-        Assert.assertTrue(response);
-        Assert.assertNotNull(this.node.lookup(this.word, 0));
+        Assert.assertEquals(validIndex, response);
+        Assert.assertNotNull(this.node.lookup(WORD_VALID, 0));
 
     }
     // ********************************************************************************
@@ -96,14 +98,14 @@ public class TrieNodeTest {
 
     @Test
     public void verify_lookup_returnsNull_whenPositionIsGreaterOrEqual_thanWordLength() {
-        TrieNode response = this.node.lookup(this.word, this.word.length());
+        TrieNode response = this.node.lookup(WORD_VALID, WORD_VALID.length());
 
         Assert.assertNull(response);
     }
 
     @Test
     public void verify_lookup_returnsNull_whenChildrenAreNull() {
-        TrieNode response = this.node.lookup(this.word, 0);
+        TrieNode response = this.node.lookup(WORD_VALID, 0);
 
         Assert.assertNull(response);
     }
@@ -111,10 +113,10 @@ public class TrieNodeTest {
     @Test
     public void verify_lookup_returnsNull_whenIsNotFound() {
         // Mock
-        this.node.insert("program", 0);
+        this.node.insert("program", 0, 1);
 
         // Test
-        TrieNode response = this.node.lookup(this.word, 0);
+        TrieNode response = this.node.lookup(WORD_VALID, 0);
 
         Assert.assertNull(response);
     }
@@ -122,12 +124,12 @@ public class TrieNodeTest {
     @Test
     public void verify_lookup_returnsNode_whenIsFound() {
         // Mock
-        this.node.insert(this.word, 0);
+        this.node.insert(WORD_VALID, 0,1);
 
         // Test
-        TrieNode response = this.node.lookup(this.word, 0);
+        TrieNode response = this.node.lookup(WORD_VALID, 0);
 
         Assert.assertNotNull(response);
     }
-    // *********************************************************************************/
+    // ********************************************************************************
 }
