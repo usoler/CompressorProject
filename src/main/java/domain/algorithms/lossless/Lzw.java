4133423e1c12dcc92a48321d0875644cc38468bc
Miguel <miguel.angel.malqui@est.fib.upc.edu>
2019-11-05 20:49:33 +0100

    Add dictionary initialization

--- src/main/java/domain/algorithms/lossless/Lzw.java
+++ src/main/java/domain/algorithms/lossless/Lzw.java
@@ -1,4 +1,32 @@
 package domain.algorithms.lossless;
 
+import domain.dataStructure.Trie;
+
+import java.io.File;
+
 public class Lzw extends Lz78 {
+
+    private static final int ASCII_LENGTH = 256;
+
+    private void iniDictionary(Trie encodingDictionary) {
+        for (int i = 0; i < ASCII_LENGTH; i++) {
+            encodingDictionary.insert(Character.toString((char)i));
+        }
+    }
+
+    @Override
+    public void encode(String file) {
+        // ENCODING WITH LZW
+        System.out.println("Encoding file with LZ78");
+        encodingDictionary = new Trie();
+        iniDictionary(encodingDictionary);
+        // super.encode(file);
+    }
+
+    @Override
+    public void decode(File file) {
+        // DECODING WITH LZW
+        System.out.println("Decoding file with LZW");
+        // super.decode(file);
+    }
 }
