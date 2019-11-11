c9375aec6360f34aad5d536627e7e484dc3d3e1a
Miguel <miguel.angel.malqui@est.fib.upc.edu>
2019-11-11 19:49:49 +0100

    Improve speed lzw

--- src/main/java/domain/algorithms/lossless/Lzw.java
+++ src/main/java/domain/algorithms/lossless/Lzw.java
@@ -22,44 +22,70 @@ public class Lzw extends Lz78 {
         }
     }
 
+    private void initializeEncodingDictionary(Trie encodingDictionary) {
+        for (int i = 0; i < ASCII_LENGTH; i++) {
+            encodingDictionary.insert(Character.toString((char)i));
+        }
+    }
+
     @Override
     public String encode(String file) {
         // ENCODING WITH LZW
         System.out.println("Encoding file with LZW");
         encodingDictionary = new Trie();
+        initializeEncodingDictionary(encodingDictionary);
+
+        StringBuilder newText = new StringBuilder();
         int i = 0;
-        String prefix = "";
-        String newText = "";
         while (i < file.length()) {
-            String nextCharacter = Character.toString(file.charAt(i));
-            if (encodingDictionary.contains(prefix + nextCharacter) || ((prefix+nextCharacter).length() == 1 && (prefix+nextCharacter).charAt(0) < ASCII_LENGTH)) {
-                prefix += nextCharacter;
+            StringBuilder word = new StringBuilder();
+            boolean found = encodingDictionary.contains(word.append(file.charAt(i)).toString());
+            while (i + 1 < file.length() && (found = encodingDictionary.contains(word.append(file.charAt(i+1)).toString()))) {
+                ++i;
+            }
+            if (!found) {
+                encodingDictionary.insert(word.toString());
+                newText.append(encodingDictionary.getIndexOf(word.deleteCharAt(word.length()-1).toString())-1);
+                newText.append(';');
             }
             else {
-                encodingDictionary.insert(prefix + nextCharacter);
-                if (prefix.length() == 1 && prefix.charAt(0) < ASCII_LENGTH) {
-                    newText += (int)prefix.charAt(0) + ";";
-                }
-                else {
-                    newText += Integer.toString(encodingDictionary.getIndexOf(prefix) + ASCII_LENGTH - 1) + ";";
-                }
-                prefix = nextCharacter;
+                newText.append(encodingDictionary.getIndexOf(word.toString())-1);
+                newText.append(';');
             }
             ++i;
         }
-        if (prefix.length() == 1 && prefix.charAt(0) < ASCII_LENGTH) {
-            newText += (int)prefix.charAt(0) + ";";
-        }
-        else {
-            newText += Integer.toString(encodingDictionary.getIndexOf(prefix) + ASCII_LENGTH - 1) + ";";
-        }
-        return newText;
+
+        return newText.toString();
     }
 
     @Override
     public String decode(String file) {
-        // DECODING WITH LZW
+        // ENCODING WITH LZW
         System.out.println("Decoding file with LZW");
+        if (file.length() == 0) {
+            return "";
+        }
+        HashMap<Integer, String> decodingDictionary = new HashMap<>();
+        initializeDecodingDictionary(decodingDictionary);
+        StringBuilder newText = new StringBuilder();
+        char firstCharacter;
+        int[] codes = Arrays.stream(file.split(";")).mapToInt(Integer::parseInt).toArray();
+        newText.append(decodingDictionary.get(codes[0]));
+        for (int i = 1; i < codes.length; i++) {
+            StringBuilder word = new StringBuilder();
+            if (decodingDictionary.containsKey(codes[i])) {
+                word.append(decodingDictionary.get(codes[i]));
+            }
+            else {
+                word.append(decodingDictionary.get(codes[i-1]));
+                word.append(word.charAt(0));
+            }
+            newText.append(word);
+            decodingDictionary.put(decodingDictionary.size(), decodingDictionary.get(codes[i-1]) + word.charAt(0));
+        }
+
+        return newText.toString();
+        /*System.out.println("Decoding file with LZW");
         if (file.length() <= 0) {
             return "";
         }
@@ -87,7 +113,6 @@ public class Lzw extends Lz78 {
             oldCode = newCode;
             ++i;
         }
-        return newText;
-
+        return newText;*/
     }
 }
