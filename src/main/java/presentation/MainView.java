0905a7269688a3ceacce469910dd8b544996b8f8
Miguel <miguel.angel.malqui@est.fib.upc.edu>
2019-11-21 14:01:40 +0100

    Add some buttons to main view

--- src/main/java/presentation/MainView.java
+++ src/main/java/presentation/MainView.java
@@ -8,9 +8,21 @@ public class MainView {
     private PresentationController presentationController;
 
     private Frame frame = new Frame("Compressor 3000");
+    private Panel contentPanel = new Panel();
+
+    private Panel leftPanel = new Panel();
+    private Button statsButton = new Button("Stats");
+    // TODO Add drag&drop component
+
+    private Panel buttonsPanel = new Panel();
     private Button addButton = new Button("+ ADD");
     private Button searchButton = new Button("Search");
     private TextField searchBox = new TextField();
+
+    // Perhaps it would be better to create a table class
+    private Panel historyPanel = new Panel();
+
+    private Panel informationPanel = new Panel();
 
     public MainView(PresentationController controller) {
         presentationController = controller;
@@ -19,7 +31,11 @@ public class MainView {
 
     private void initializeComponents() {
         initializeFrame();
-
+        initializeContentPanel();
+        initializeLeftPanel();
+        initializeButtonsPanel();
+        initializeHistoryPanel();
+        initializeInformationPanel();
     }
 
     private void initializeFrame() {
@@ -29,12 +45,31 @@ public class MainView {
                 System.exit(0);
             }
         });
-
+        frame.add(contentPanel);
     }
 
+    private void initializeContentPanel() {
+        contentPanel.setLayout(new FlowLayout());
+        contentPanel.add(buttonsPanel);
+    }
+
+    private void initializeLeftPanel() {
+    }
+
+    private void initializeButtonsPanel() {
+        buttonsPanel.setLayout(new FlowLayout());
+        buttonsPanel.add(addButton);
+        buttonsPanel.add(searchBox);
+        buttonsPanel.add(searchButton);
+    }
+
+    private void initializeHistoryPanel() {}
+
+    private void initializeInformationPanel() {}
+
     public void setVisible() {
-        //frame.pack();
-        frame.setSize(640, 480);
+        frame.pack();
+        //frame.setSize(640, 480);
         frame.setVisible(true);
     }
 }
