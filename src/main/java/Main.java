0edcd38c600db1561248c56c1a79cdd865480330
Leeggnna <angel.gon.jur@gmail.com>
2019-12-15 22:21:57 +0100

    Revert minor change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
-import domain.IFile;
+
 import domain.exception.CompressorException;
-import data.fileManager.FileManager;
+
+import presentation.PresentationController;
+
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) throws CompressorException {
-        /*
+
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -14,15 +17,7 @@ public class Main {
             }
         });
 
-         */
-        FileManager fileManager = new FileManager();
-        fileManager.readFolder("input");
-        IFile file = fileManager.getFile("input/test.txt");
-        IFile file2 = fileManager.getFile("input/test2.txt");
-        IFile folder1 = fileManager.getFile("input/testFolder");
-        System.out.println(file.getName());
-        System.out.println(file2.getName());
-        System.out.println(folder1.getName());
+
 
     }
 }
